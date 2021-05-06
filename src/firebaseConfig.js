987adb19e9d831ffcfdc74eb7daa654c6db4865e
mjhasan <59266447+mjhasan@users.noreply.github.com>
2021-05-06 23:47:04 +0600

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,3 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyC0clC9QZEqnyPAanI7fsH4Sv9xvuM9Sss",
-    authDomain: "creative-agency-88651.firebaseapp.com",
-    projectId: "creative-agency-88651",
-    storageBucket: "creative-agency-88651.appspot.com",
-    messagingSenderId: "899778278897",
-    appId: "1:899778278897:web:147728ce2ff271c63b8050"
+    Add your firebase config here
   };
